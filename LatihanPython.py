1dfafedd0a8ea7546dbf3a2bb72ba935fa1ff7e2
Imam Solehudin <imamsolehudin59@gmail.com>
2021-10-27 17:58:18 +0700

    Menambahkan LatihanPython.py

---
+++ LatihanPython.py
@@ -0,0 +1,20 @@
+print("Hello")
+print("Saya sedang belajar python")
+
+a = 8
+b = 6
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penjumlahan a+b=",a+b)
+
+a=int(input("masukan nilai a:"))
+b=int(input("masukan nilai b:"))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {0}&{1}=%d".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {0}+{1}=%d".format(a,b) %(a+b))
+print("hasil pembagian {0}/{1}=%d".format(a,b) %(a/b))
